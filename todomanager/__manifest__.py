ca14ed7970ded3d38e6a87dafe478f334eed729a
Taiel <tdeluca@potenciardesarrollos.com>
2024-05-09 20:39:00 -0300

    Initial commit

---
+++ todomanager/__manifest__.py
@@ -0,0 +1,11 @@
+{
+	'name':'To Do Manager',
+	'author':'Taiel De Luca',
+	'website': 'taieldeluca.com.ar',
+    'summary':'Odoo 16 Dev test',
+    'data':[
+        "security/ir.model.access.csv",
+        'views/task.xml',
+        'views/menu.xml',
+	]
+}
